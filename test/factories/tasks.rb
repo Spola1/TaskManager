5654e1e68fb64d44e189b104228e858558218552
Spola1 <1111.22224@mail.ru>
2022-09-24 15:50:35 +0300

    fix test

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :task do
     name
     description
-    author_id { create(:manager).id }
-    assignee_id { create(:developer).id }
+    author factory: :manager
+    assignee factory: :developer
     state
     expired_at
   end

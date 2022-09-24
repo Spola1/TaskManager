cfefd048270fdc1407451828325cb598ccec62a7
Spola1 <1111.22224@mail.ru>
2022-09-24 10:20:28 +0300

    tests

---
+++ test/factories/sequence.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar,
+      :type, :name, :state] do |n|
+    "string#{n}"
+  end
+
+  sequence :text, aliases: [:description] do |n|
+    "example#{n}"
+  end
+
+  sequence :date, aliases: [:expired_at] do |n|
+    "#{n}-01-01"
+  end
+end

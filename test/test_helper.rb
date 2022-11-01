1d6d111e0bcbde4298220ad09ae915549a7599b6
Spola1 <1111.22224@mail.ru>
2022-11-01 11:43:09 +0300

    update readme.md && test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,10 @@
-require 'simplecov'
-SimpleCov.start
+if ENV['CI']
+  require 'simplecov'
+  SimpleCov.start 'rails'
 
-require 'coveralls'
-Coveralls.wear!('rails')
+  require 'coveralls'
+  Coveralls.wear!('rails')
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
